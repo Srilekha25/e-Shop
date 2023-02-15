49ddced3d0567375f0efd740d1a47082590e0eba
Srilekha25 <kasaragaddasrilekha@gmail.com>
2023-02-15 23:59:20 +1100

    hosting

--- deploy.sh
+++ deploy.sh
@@ -1,1 +1,10 @@
-# git push -f git@hithub.com:Srilekha25/e-Shop.git main:gh-pages
+set -e
+npm run build
+cd dist
+echo > .nojekyll
+
+git init
+git checkout -B main
+git add -A
+git commit -m 'deploy'
+# git push -f git@github.com:Srilekha25/e-Shop.git main:gh-pages
